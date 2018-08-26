6f813f1f2a5535a1b0abbbfa1969830949ee56d5
github username <github email address>
2018-08-26 16:54:46 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,19 +1,3 @@
-# class Application
-#
-#   def call(env)
-#     resp = Rack::Response.new
-#   #   t = Time.now
-#   #   if t.hour < 12
-#   #     resp.write(Morning)
-#   #   else
-#   #     resp.write(Afternoon)
-#   #   end
-#  end
-#
-#
-#   resp.finish
-# end
-
 class Application
 
   def call(env)
