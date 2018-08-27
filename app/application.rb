b154f51218eb134a2d0e1bfb11873265ee486f20
Yixuan Wang <ywang0506@gmail.com>
2018-08-27 06:43:00 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,13 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    now = Time.now
+    if now.hour < 12
+      resp.write "Good Morning"
+    else
+      resp.write "Good Afternoon"
+    end
+    resp.finish
+  end
+end
